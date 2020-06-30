455acf2ad44efb708a29e1f19ed5cb8d3261991f
Mercy1 <taylorangel887@gmail.com>
2020-06-30 17:28:09 +0100

    Update __init__.py

--- tempvoice/__init__.py
+++ tempvoice/__init__.py
@@ -1,23 +1,4 @@
-import asyncio
-import logging
-from abc import ABCMeta
-from datetime import timedelta
-from typing import Optional
-
-from discord.ext.commands import CogMeta as DPYCogMeta
-from redbot.core import Config, commands
-
-log = logging.getLogger("red.shino.relays")
-
-
-# This previously used ``(type(commands.Cog), type(ABC))``
-# This was changed to be explicit so that mypy would be slightly happier about it.
-# This does introduce a potential place this can break in the future, but this would be an
-# Upstream breaking change announced in advance
-
-
-
-from .tempvoice import voice
+from .tempvoice import Voice
 
 def setup(bot):
     bot.add_cog(voice())
