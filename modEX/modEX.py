f96a6166c12f216435d5db72cf5f392c7e512f04
Mercy1 <taylorangel887@gmail.com>
2020-07-02 15:29:58 +0100

    Added class tempmute

--- modEX/modEX.py
+++ modEX/modEX.py
@@ -13,6 +13,7 @@ from .events import Events
 from .kickban import KickBanMixin
 from .movetocore import MoveToCore
 from .mutes import MuteMixin
+from .mutes import tempmute
 from .names import ModInfo
 from .slowmode import Slowmode
 from .settings import ModSettings
@@ -38,6 +39,7 @@ class ModEX(
     KickBanMixin,
     MoveToCore,
     MuteMixin,
+    tempmute,
     ModInfo,
     Slowmode,
     commands.Cog,
