18b221cfba8fd3d2eb2d980f7da343574502e365
Mercy1 <taylorangel887@gmail.com>
2020-08-11 20:14:01 +0100

    Revert "Updated regex to include youtube"

--- gallery/gallery.py
+++ gallery/gallery.py
@@ -88,13 +88,15 @@ class Gallery(commands.Cog):
         user = message.author
         if user.bot:
             return
+        if message.youtube:
+            return
         if not message.guild:
             return
         if message.channel.id not in await self.config.guild(message.guild).channels():
             return
         if not message.attachments:
             uris = re.findall(
-                "(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})",
+                "http[s]?://(?:|(?:youtube.com/|[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F])))+",
                 message.content,
             )
             if len(uris) == 1:
@@ -103,7 +105,8 @@ class Gallery(commands.Cog):
                 parts = uri.split(".")
                 extension = parts[-1]
                 imageTypes = ["jpg", "jpeg", "tiff", "png", "gif", "bmp", "mp4", "webm"]
-                if extension in imageTypes:
+                Youtube = ["^([a-z|A-Z])+?://([^/]+[.])?(youtube[.]com|YOUTUBE[.]COM)?(/.*)?$"]
+                if extension in imageTypes or Youtube:
                     return
             rid = await self.config.guild(message.guild).whitelist()
             time = await self.config.guild(message.guild).time()
