ec27cad7f18abcc46e653d20a6cb785fae1d3a1c
Mercy1 <taylorangel887@gmail.com>
2020-08-11 23:04:00 +0100

    Update gallery.py

--- gallery/gallery.py
+++ gallery/gallery.py
@@ -94,7 +94,7 @@ class Gallery(commands.Cog):
             return
         if not message.attachments:
             uris = re.findall(
-                "(?!\S+youtube\.com)((?<!\S)(((f|ht){1}tp[s]?:\/\/|(?<!\S)www\.)[-a-zA-Z0-9@:%_\+.~#?&\/\/=]+))",
+                "(?=\S+youtube\.com)((?<!\S)(((f|ht){1}tp[s]?:\/\/|(?<!\S)www\.)[-a-zA-Z0-9@:%_\+.~#?&\/\/=]+))",
                 message.content,
             )
             if len(uris) == 1:
