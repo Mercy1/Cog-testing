f1c931e316f1c02cba052347a6791530e0587768
Mercy1 <taylorangel887@gmail.com>
2020-08-18 23:46:01 +0100

    Match data GDPR/ DIscord ruling

--- gallery/__init__.py
+++ gallery/__init__.py
@@ -1,5 +1,10 @@
 from .gallery import Gallery
 
+__red_end_user_data_statement__ = (
+    "This cog does not store any user data, all this cog is actively responsable for is allowing image only channels.\n"
+    "It will not respect data deletion by end users as no data is stored.\n"
+)
+
 
 def setup(bot):
     bot.add_cog(Gallery(bot))
