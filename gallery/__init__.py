aec9eaff11554d73ccfd1b8cef146ee69d3b4699
Mercy1 <taylorangel887@gmail.com>
2020-08-19 00:12:23 +0100

    Update __init__.py

--- gallery/__init__.py
+++ gallery/__init__.py
@@ -1,10 +1,5 @@
 from .gallery import Gallery
 
-__red_end_user_data_statement__ = (
-    "This cog does not store any user data, all this cog is actively responsable for is allowing image only channels."
-    "It will not respect data deletion by end users as no data is stored."
-)
-
 
 def setup(bot):
     bot.add_cog(Gallery(bot))
